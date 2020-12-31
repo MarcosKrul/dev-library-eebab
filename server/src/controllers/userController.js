4279b6b4b9ab93dc5b99d4906d22742671cb0e1e
Marcos Renan Krul <marcoskrul2k@gmail.com>
2020-12-31 13:07:54 -0300

    be: controller for users

---
+++ server/src/controllers/userController.js
@@ -0,0 +1,126 @@
+const knex = require('../database/index')
+const bcrypt = require('bcrypt')
+const emailValidator = require('email-validator')
+const transporterConfig = require('../config/transporterEmail')
+require('dotenv/config')
+
+module.exports = {
+    async index(req, res)  {
+        const results = await knex('users')
+        results.map((user) => {
+            user.password = undefined
+        })
+        return res.json(results)
+    },
+    async create(req, res) {
+        
+        const { 
+            name, 
+            type, 
+            email, 
+            avatar, 
+            password, 
+        } = req.body
+
+        if(!password) return res.status(400).send({
+            error: 'password is null'
+        })
+        if(password.length > 45) return res.status(400).send({
+            error: 'password exceed 45 char'
+        })
+        if(email.length > 45) return res.status(400).send({
+            error: 'email exceed 45 char'
+        })
+        if(!emailValidator.validate(email)) return res.status(400).send({
+            error: 'email is not valid'
+        })
+        if(name.length > 45) return res.status(400).send({
+            error: 'name exceed 45 char'
+        })
+
+        const result = knex('users').where('email', '=', email)
+        if((await result).length !== 0) return res.status(400).send({
+            error: 'user already exists'
+        })
+
+        const salt = bcrypt.genSaltSync(10)
+        const hash = bcrypt.hashSync(password, salt)
+        await knex('users').insert({
+            name: name,
+            email: email,
+            avatar: avatar,
+            password: hash,
+            user_type: type,
+            created_at: new Date(),
+        }).catch((error) => {
+            return res.status(400).send({
+                error: error.message
+            })
+        })
+
+        transporterConfig('welcome').sendMail({
+            from: process.env.NODEMAILER_EMAIL,
+            to: email,
+            subject: 'Confirmação',
+            template: 'index',
+            context: {
+                name: name.toUpperCase(),
+            }
+        }).then(() => {
+            return res.status(200).send()
+        }).catch(error => {
+            return res.status(400).send({
+                error: error.message
+            })
+        })
+    },
+    async delete(req, res) {
+        const { id } = req.params
+        await knex('users').where('id', '=', id).del().then((data) => {
+            if(data === 0) return res.status(404).send({
+                error: 'user not found'
+            })
+        }).catch((error) => {
+            return res.status(400).send({
+                error: error.message
+            })
+        })
+        return res.status(200).send()
+    },
+    async avatarUpdate(req, res) {
+        const { id, avatar } = req.body
+
+        if(id !== req.id) return res.status(400).send({
+            error: `changing another user's data`
+        })
+
+        await knex('users')
+            .update( {avatar: avatar} )
+            .where('id', '=', id)
+            .then(() => {
+                return res.status(200).send()
+            }).catch((error) => {
+                return res.status(400).send({
+                    error: error.message
+                })
+            })
+    },
+    async nameUpdate(req, res) {
+        const { id, name } = req.body
+
+        if(id !== req.id) return res.status(400).send({
+            error: `changing another user's data`
+        })
+
+        await knex('users')
+            .update( {name: name} )
+            .where('id', '=', id)
+            .then(() => {
+                return res.status(200).send()
+            }).catch((error) => {
+                return res.status(400).send({
+                    error: error.message
+                })
+            })
+    }
+}
