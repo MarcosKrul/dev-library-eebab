877b1dbd860bc669b2f8cfe48751e4bb675274a1
Marcos Renan Krul <marcoskrul2k@gmail.com>
2020-12-31 13:00:18 -0300

    be: middleware for user auth

---
+++ server/src/middlewares/authLogin.js
@@ -0,0 +1,31 @@
+const jwt = require('jsonwebtoken')
+require('dotenv/config')
+
+module.exports = (req, res, next) => {
+    const authHeader = req.headers.authorization
+    if(!authHeader) return res.status(401).send({
+        error: 'no token provided'
+    })
+
+    const parts = authHeader.split(' ')
+    if(!parts.length === 2) return res.status(401).send({
+        error: 'token error'
+    })
+
+    const [ scheme, token ] = parts
+    if(!/^Bearer$/i.test(scheme)) return res.status(401).send({
+        error: 'token malformatted'
+    })
+
+    jwt.verify(token, 
+        process.env.SECRET_KEY_TOKEN, 
+        (err, decoded) => {
+            if(err) return res.status(401).send({
+                error: 'token invalid'
+            })
+            req.id = decoded.id
+            req.type = decoded.admin
+            req.email = decoded.email
+        return next()
+    })
+}
