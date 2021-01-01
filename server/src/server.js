2c193fa530aad0277fd036451dbba530e050e23a
Marcos Renan Krul <marcoskrul2k@gmail.com>
2020-12-31 23:59:24 -0300

    be: fix connection with fe

--- server/src/server.js
+++ server/src/server.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const path = require('path')
+const cors = require('cors')
 const routes = require('./routes/index')
 
 const app = express()
-app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp', 'uploads')))
+app.use(cors())
 app.use(express.json())
 app.use(routes)
+
+app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp', 'uploads')))
+
 app.listen(3333)
