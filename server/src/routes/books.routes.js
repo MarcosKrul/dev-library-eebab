b839042ebd26b58b5193e6fb1e405779bb86adac
Marcos Renan Krul <marcoskrul2k@gmail.com>
2021-01-05 19:37:11 -0300

    be: book services

--- server/src/routes/books.routes.js
+++ server/src/routes/books.routes.js
@@ -1,16 +1,21 @@
 const routes = require('express').Router()
 const bookController = require('../controllers/bookController')
 
+const bookServices = require('../services/books')
+
 const authLoginMiddleware = require('../middlewares/authLogin')
+const adminOnlyMiddleware = require('../middlewares/admOnly')
 const managerOnlyMiddleware = require('../middlewares/managerOnly')
 
+routes.post('/books/loan',                authLoginMiddleware, adminOnlyMiddleware,   bookServices.loan)
 routes.get('/books',                      authLoginMiddleware,                        bookController.index)
 routes.post('/books',                     authLoginMiddleware, managerOnlyMiddleware, bookController.create)
 routes.delete('/books/:id',               authLoginMiddleware, managerOnlyMiddleware, bookController.delete)
+routes.post('/books/devolution',          authLoginMiddleware, adminOnlyMiddleware,   bookServices.devolution)
 routes.patch('/books/name/update',        authLoginMiddleware, managerOnlyMiddleware, bookController.nameUpdate)
 routes.patch('/books/author/update',      authLoginMiddleware, managerOnlyMiddleware, bookController.authorUpdate)
 routes.patch('/books/amount/update',      authLoginMiddleware, managerOnlyMiddleware, bookController.amountUpdate)
-routes.patch('/books/book-cover/update', authLoginMiddleware, managerOnlyMiddleware, bookController.bookCoverUpdate)
+routes.patch('/books/book-cover/update',  authLoginMiddleware, managerOnlyMiddleware, bookController.bookCoverUpdate)
 
 
 module.exports = routes
---
+++ server/src/services/books.js
@@ -0,0 +1,88 @@
+const knex = require('../database/index')
+
+module.exports = {
+    async loan(req, res) {
+        const { book_id, user_id } = req.body
+
+        if(!book_id || !user_id) return res.status(401).send({
+            error: 'some arguments are null'
+        })
+
+        const result = await knex('book_loan')
+            .where('book_id', '=', book_id)
+            .where('user_id', '=', user_id)
+        if(result.length !== 0) return res.status(401).send({
+            error: 'loan already exists'
+        })
+
+        const users = await knex('users').where('id', '=', user_id)
+        if(users.length === 0) return res.status(404).send({
+            error: 'user not found'
+        })
+
+        const books = await knex('books').where('id', '=', book_id)
+        if(books.length === 0) return res.status(404).send({
+            error: 'book not found'
+        })
+
+        if(books[0].amount === 0) return res.status(401).send({
+            error: 'out of stock'
+        })
+        
+        await knex('book_loan').insert({
+            ticket: 0,
+            user_id: user_id,
+            book_id: book_id,
+            created_at: new Date()
+        }).catch((error) => {
+            return res.status(500).send({
+                error: error.message
+            })
+        })
+
+        await knex('books')
+            .where('id', '=', books[0].id)
+            .decrement({
+                amount: 1
+            }).then(() => {
+                return res.status(200).send()
+            }).catch((error) => {
+                return res.status(500).send({
+                    error: error.message
+                })
+            })
+    }, 
+    async devolution(req, res){
+        const { user_id, book_id } = req.body
+
+        if(!user_id || !book_id) return res.status(401).send({
+            error: 'some arguments are null'
+        })
+
+        try {
+            const result = await knex('book_loan')
+                .where('book_id', '=', book_id)
+                .where('user_id', '=', user_id)
+
+            if(result.length === 0) return res.status(404).send({
+                error: 'loan not found'
+            })
+
+            if(Number(result[0].ticket) !== 0) return res.status(401).send({
+                error: 'user has pending tickets'
+            })
+
+            await knex('book_loan').where('id', '=', result[0].id).del()
+                
+            await knex('books').where('id', '=', book_id).increment({
+                amount: 1
+            })
+                
+            return res.status(200).send()
+        } catch(err) {
+            return res.status(500).send({
+                error: err.message
+            })
+        }
+    }
+}
